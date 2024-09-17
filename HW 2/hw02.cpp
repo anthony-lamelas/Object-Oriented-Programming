8bab21ee432bf3bfa223cc72ebc400abc19060bb
anthony-lamelas <anthony.lamelas23@gmail.com>
2024-09-16 23:10:20 -0400

    finished finally

--- HW 2/hw02.cpp
+++ HW 2/hw02.cpp
@@ -19,7 +19,7 @@ struct Warrior{
     int strength;
 };
 
-// WarriorList struct with list of all warriors
+// WarriorList struct with vector of all warriors
 struct WarriorList{
     vector<Warrior> Warriors;
 };
@@ -28,6 +28,8 @@ struct WarriorList{
 void battle(Warrior& Warrior1, Warrior& Warrior2);
 void status(const vector<Warrior>& Warriors);
 void run_file(fstream& warriors_file);
+void add_warrior(fstream& warriors_file, WarriorList& warriorlist);
+void execute_battle(fstream& warriors_file, WarriorList& warriorlist);
 
 // Main 
 int main(){
@@ -35,15 +37,13 @@ int main(){
     run_file(warriors_file);
 }
 
-// Battle function that takes in two warriors
+// Battle function that takes in two warriors, runs the battle, returns nothing 
 void battle(Warrior& Warrior1, Warrior& Warrior2){
     if (Warrior1.name.empty() || Warrior2.name.empty()){
         cerr << "Error: At least one of the warriors is not initialized." 
         << endl;
-    }
-    else {
+    } else {
          cout << Warrior1.name << " battles " << Warrior2.name << endl;
-
         // Case where Warrior1 exists and Warrior2 does not
         if (Warrior1.strength > 0 && Warrior2.strength == 0){  
             cout << "He's dead, " << Warrior1.name << endl;
@@ -78,8 +78,8 @@ void battle(Warrior& Warrior1, Warrior& Warrior2){
         }
     }
 }
-
 // Status command displays how many warriors and their amount of strength
+// Takes in a vector of warriors, returns nothing
 void status(const vector<Warrior>& Warriors){
     size_t amount_warriors = Warriors.size();
     cout << "There are: " << amount_warriors << " warriors" << endl;
@@ -90,7 +90,8 @@ void status(const vector<Warrior>& Warriors){
     }
 }
 
-// Function to open and iterate through the file
+// Function to open and iterate through the file, takes in an fstream
+// Returns nothing
 void run_file(fstream& warriors_file){
     warriors_file.open("warriors.txt");
 
@@ -103,59 +104,59 @@ void run_file(fstream& warriors_file){
     string command;
 
     while(warriors_file >> command){
-        // Creates new Warrior
         if (command == "Warrior"){ 
-            string name;
-            int strength;
-            warriors_file >> name >> strength;
-            bool duplicate = false;
-
-            // Checks for duplicates
-            for (const Warrior& warrior : warriorlist.Warriors) { 
-                if (warrior.name == name) {
-                    cerr << "Error: Warrior with the name '" << name 
-                    << "' already exists" << endl;
-                    duplicate = true;
-                    break;
-                }
-            }
-            
-            // If no duplicate, add the new warrior
-            if (!duplicate) { 
-                Warrior new_warrior;
-                new_warrior.name = name;
-                new_warrior.strength = strength;
-                warriorlist.Warriors.push_back(new_warrior);
-            }
-
-        // Handles the Battle command
+            add_warrior(warriors_file, warriorlist);
         } else if (command == "Battle"){ 
-            string name1, name2;
-            warriors_file >> name1 >> name2;
-            Warrior* Warrior1 = nullptr;
-            Warrior* Warrior2 = nullptr;
-
-            // Find the warriors by name
-            for (size_t i = 0; i < warriorlist.Warriors.size(); ++i) {
-                if (warriorlist.Warriors[i].name == name1) {
-                    Warrior1 = &warriorlist.Warriors[i];
-                }
-                if (warriorlist.Warriors[i].name == name2) {
-                    Warrior2 = &warriorlist.Warriors[i];
-                }
-            }
-
-             // If warriors found, run the battle function
-            if (Warrior1 && Warrior2) {
-                battle(*Warrior1, *Warrior2);
-            } else {
-                cerr << "Error: One or both warriors not found." << endl;
-            }
-
-        // Runs status function
+            execute_battle(warriors_file, warriorlist);
         } else if (command == "Status"){ 
             status(warriorlist.Warriors);
         }
     }
     warriors_file.close();
+}
+
+// Function to add a new warrior to the list, takes in fstream and vector of 
+// warriors, returns nothing
+void add_warrior(fstream& warriors_file, WarriorList& warriorlist) {
+    string name;
+    int strength;
+    warriors_file >> name >> strength;
+    bool duplicate = false;
+
+    for (const Warrior& warrior : warriorlist.Warriors) {
+        if (warrior.name == name) {
+            cerr << "Error: Warrior with the name '" << name 
+                 << "' already exists" << endl;
+            duplicate = true;
+            break;
+        }
+    }
+    if (!duplicate) {
+        Warrior new_warrior{name, strength};
+        warriorlist.Warriors.push_back(new_warrior);
+    }
+}
+
+// Function to execute a battle between two warriors, takes in fstream and 
+// vector of warriors, returns nothing
+void execute_battle(fstream& warriors_file, WarriorList& warriorlist) {
+    string name1, name2;
+    warriors_file >> name1 >> name2;
+    size_t index1 = warriorlist.Warriors.size();
+    size_t index2 = warriorlist.Warriors.size();
+
+    for (size_t i = 0; i < warriorlist.Warriors.size(); ++i) {
+        if (warriorlist.Warriors[i].name == name1) {
+            index1 = i;
+        }
+        if (warriorlist.Warriors[i].name == name2) {
+            index2 = i;
+        }
+    }
+    if (index1 != warriorlist.Warriors.size() 
+    && index2 != warriorlist.Warriors.size()) {
+        battle(warriorlist.Warriors[index1], warriorlist.Warriors[index2]);
+    } else {
+        cerr << "Error: One or both warriors not found." << endl;
+    }
 }
