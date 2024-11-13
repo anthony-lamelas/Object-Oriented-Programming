414c1fc0ddde834785a30f106710a51e8b881f34
anthony-lamelas <anthony.lamelas23@gmail.com>
2024-11-13 14:48:02 -0500

    hw8 progress

--- HW 8/hw08.cpp
+++ HW 8/hw08.cpp
@@ -1,8 +1,8 @@
 /*
   Anthony Lamelas
-  Noble and Warrior program, Nobles can fight or have armies with protectors,
-  protectors can be wizards or warriors
-  Warriors can be archers or swordsmen
+  Noble and protector program, Nobles can fight or have armies with protectors,
+  protectors can be wizards or protectors
+  protectors can be archers or swordsmen
   CS2124 hw08.cpp
   -jbs
 
@@ -50,13 +50,13 @@ class Lord : public Noble {
   // Gets the army size
     size_t getSize() const;
 
-    //Removes Warrior from army
+    //Removes protector from army
     bool remove_protector(Protector& protector);
 
-      // Hire Warrior
+      // Hire protector
     bool hires(Protector& protector);
 
-    // Fire Warrior
+    // Fire protector
     bool fire(Protector& protector);
 
     // Gets the total strength of the noble's army
@@ -73,13 +73,13 @@ class Lord : public Noble {
 class PersonWithStrengthToFight : public Noble {
   public:
 
-   PersonWithStrengthToFight(const string& name, int strength);
+   PersonWithStrengthToFight(const string& name, const double& strength);
 
 
   void battle(Noble& opponent) override;
 
   private:
-    int strength;
+    double strength;
 };
 
 class Protector {
@@ -108,18 +108,18 @@ class Wizard : public Protector {
 
 };
 
-class Warrior : public Protector {
-  Warrior(const string& name, double str);
+class protector : public Protector {
+  protector(const string& name, double str);
 
 
 };
 
-class Archer : public Warrior {
+class Archer : public protector {
   Archer(const string& name, double str);
 
 };
 
-class Swordsman : public Warrior {
+class Swordsman : public protector {
   Swordsman(const string& name, double str);
 
 
@@ -173,3 +173,183 @@ int main() {
 	 << "==========\n";
 } // main
 
+
+Noble::Noble(const string& name) : noble_name(name), living(true) {}
+
+// Gets the name
+const string& Noble::getName() const { return noble_name; }
+
+// Gets the life status
+bool Noble::getStatus() const { return living; }
+
+void Noble::kill() {
+        living = false;
+        // Makes all protector strengths 0 
+        for (Protector* const& protector : army) {
+            protector->setStrength(0);
+            protector->setBoss(nullptr);
+        }
+    }
+
+Lord::Lord(const string& name) : Noble(name){}
+
+size_t Lord::getSize() const { return army.size(); }
+
+// Gets the total strength of the noble's army
+const double Lord::getArmyStrength() const {
+        double total_str = 0;
+        for ( protector* const& protector : army) {
+            total_str += protector->getStrength();
+        }
+        return total_str;
+    }
+
+// Updates army strength after the battle
+void Lord::setArmyStrength(double champ_str, double loser_str) {
+        double str_ratio = loser_str / champ_str;
+        for (protector* const protector : army) {
+            double new_str = protector->getStrength() * (1 - str_ratio);
+            protector->setStrength(new_str);
+        }
+    }
+// Hire protector
+bool Lord::hires(Protector& protector) {
+        if (protector.isHired() || !living) {
+            cout << noble_name << " failed to hire " << protector.getName() 
+            << endl;
+            return false;
+        } else {
+            protector.changeJobStatus();
+            army.push_back(&protector);
+            protector.setBoss(this);
+            return true;
+        }
+    }
+
+// Fire protector
+bool Lord::fire(Protector& protector) {
+    if (!living) {
+        return false;
+    }
+    if (remove_protector(protector)){
+        cout << protector.getName() << ", you don't work for me anymore  ! -- " 
+        << noble_name << "." << endl;
+        return true;
+        }
+
+        return false;
+}
+
+void Lord::battle(Noble& opponent) {
+        cout << noble_name << " battles " << opponent.getName() << endl;
+        if (!living && !opponent.getStatus()) { // Both dead
+            cout << "Oh, NO! They're both dead! Yuck!" << endl;
+        }
+        
+        else if (!living && opponent.getStatus()) { // Main dead
+            cout << "He's dead, " << opponent.getName() << endl;
+        }
+        
+        else if (living && !opponent.getStatus()) { // Enemy is dead
+            cout << "He's dead, " << noble_name << endl;
+        }
+        
+        else { // Both are alive
+            double my_str = getArmyStrength();
+            double opponent_str = opponent.getArmyStrength();
+            if (my_str == opponent_str) { // If it is a tie
+                kill();
+                opponent.kill();
+                cout << "Mutual Annihilation: " << noble_name << " and "
+                    << opponent.getName() << " die at each other's hands"
+                    << endl;
+            }
+            else if (my_str > opponent_str) { // If main noble wins
+                setArmyStrength(my_str, opponent_str);
+                opponent.kill();
+                cout << noble_name << " defeats " << opponent.getName()
+                    << endl;
+            }
+            else { // If opponent wins
+                opponent.setArmyStrength(opponent_str, my_str);
+                kill();
+                cout << opponent.getName() << " defeats " << noble_name
+                    << endl;
+            }
+        }
+    }
+
+bool Lord::remove_protector(Protector& protector) {
+    bool found = false;
+    int index;
+
+    for (size_t i = 0; i < army.size(); i++){
+        if (army[i] == &protector){
+            protector.changeJobStatus();
+            protector.setBoss(nullptr);
+            found = true;
+            index = i;
+            break;
+        }
+    }
+    if (found) {
+            for (size_t i = index; i < army.size() - 1; i++) {
+                army[i] = army[i + 1];
+            }
+            army.pop_back();
+            return true;
+    }
+    return false;
+}   
+
+PersonWithStrengthToFight::PersonWithStrengthToFight(const string& name, 
+const double& str) : Noble(name), strength(str) {}
+
+void PersonWithStrengthToFight::battle(Noble& opponent){
+
+}
+
+Protector::Protector(const string& name, double str) : protector_name(name), 
+strength(str){}
+
+const string& Protector::getName() const { return protector_name; }
+double Protector::getStrength() const { return strength; }
+void Protector::setStrength(double newStrength) { strength = newStrength; }
+bool Protector::isHired() const { return has_job; }
+void Protector::changeJobStatus() { has_job = !has_job; }
+void Protector::setBoss(Noble* noblep) { boss = noblep; }
+Noble* Protector::getBoss() const  {return boss; }
+
+bool Protector::runaway() {
+        if (strength == 0) {  // fail if Protector is dead
+            cout << protector_name << " is already dead!" << endl;
+            return false;
+        }
+        if (!boss) {  // fail if boss does not exist
+            cout << protector_name << " has no boss.";
+            return false;
+        }
+        string lord_name = boss->getName();
+        if (boss->remove_protector(*this)) {
+            cout << protector_name << " flees in terror, abandoning his lord, "
+                 << lord_name << endl;
+            return true;
+        }
+        return false;  
+    }
+
+
+
+//Noble Operator
+
+//Lord Operator
+
+//PersonWithStrengthToFight Operator
+
+//Protector Operator
+
+//Wizard Operator
+
+//Warroir Operator
+
+//Archer Operator
