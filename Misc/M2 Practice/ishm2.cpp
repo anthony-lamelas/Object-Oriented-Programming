301f3859ff3cc844c8b11847c19c02d789d262d5
anthony-lamelas <anthony.lamelas23@gmail.com>
2024-11-16 17:31:40 -0500

    fininshed ish Misc

--- Misc/M2 Practice/ishm2.cpp
+++ Misc/M2 Practice/ishm2.cpp
@@ -61,3 +61,99 @@ using namespace std;
 //     Cat* catP = petP; error because cannot point up on hierarchy. Cat cannot go up to pet
 //     catP->eat();
 // }
+
+// class Derived; // Yes, we need this.
+// class Base {
+// public:
+// virtual void method(Base& arg) {
+// cout << "Base::method(Base)\n";
+// }
+// virtual void method(Derived& arg) {
+// cout << "Base::method(Derived)\n";
+// }
+// };
+// class Derived : public Base {
+// public:
+// void method(Base& arg) {
+// cout << "Derived::method(Base)\n";
+// }
+// void method(Derived& arg) {
+// cout << "Derived::method(Derived)\n";
+// }
+// };
+// void someFunc(Base& argA, Base& argB) {
+// argA.method(argB);
+// }
+// int main() {
+// Derived d;
+// Base b;
+// someFunc(d, b);
+// }
+
+// class FlyingMachine {
+// public:
+// FlyingMachine() {}
+// virtual void fly() { cout << "In FlyingMachine fly()"; }
+// };
+// class HangGlider : public FlyingMachine {
+// public:
+// virtual void crash() { cout << "In HangGlider crashing()"; }
+// void fly() { cout << "In HangGlider fly()"; }
+// };
+
+// int main() {
+// HangGlider hanger;
+// FlyingMachine flier;
+// flier = hanger;
+// flier.crash();
+// }
+
+
+// class FederationStarship {
+// public:
+// FederationStarship() {}
+// void attack(string weapon) {
+// cout << "FederationStarship firing " << weapon;
+// }
+// };
+// class Constitution : public FederationStarship {
+// public:
+// virtual void transport() { cout << "Beam me up!"; }
+// void attack() {
+// cout << "Constitution firing photon torpedos";
+// }
+// };
+
+// int main() {
+// Constitution* NCC_1701 = new Constitution();
+// NCC_1701->attack("phasers");
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// class Member {
+// public:
+//     Member() { cout << 'a'; }
+//     ~Member() { cout << 'b'; }
+// };
+
+// class Base {
+//     Member member;
+
+// public:
+//     Base() { cout << 'c'; }
+//     ~Base() { cout << 'd'; }
+// };
+
+// class Derived : public Base {
+//     Member mem;
+// public:
+//     Derived() { cout << 'e'; }
+//     ~Derived() { cout << 'f'; }
+// };
+
+// int main() {
+//     Derived der;
+//     return 0;
+// }
