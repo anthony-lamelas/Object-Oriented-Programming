2ffe085c0795439165bdabe462c759168399d9b8
anthony-lamelas <anthony.lamelas23@gmail.com>
2024-11-20 13:13:06 -0500

    HW 9 Setup

---
+++ HW 9/polynomial.cpp
@@ -0,0 +1,14 @@
+/*
+Anthony Lamelas, polynomial.cpp file for polynomai linked list homework
+*/
+
+
+#include <iostream>
+#include <vector>
+#include "polynomial.h"
+using namespace std;
+
+namespace linkedPolynomial{
+
+    
+}
---
+++ HW 9/polynomial.h
@@ -0,0 +1,22 @@
+/*
+Anthony Lamelas, polynomial.h file for polynomai linked list homework
+*/
+
+#ifndef POLYNOMIAL_H
+#define POLYNOMIAL_h
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace linkedPolynomial{
+
+    class Polynomial{
+
+        public:
+
+    };
+
+}
+
+#endif
