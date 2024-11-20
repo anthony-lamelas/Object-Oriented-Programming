dba449169c76c5eb6891a29a547fefa2ab56a2e6
anthony-lamelas <anthony.lamelas23@gmail.com>
2024-11-20 16:51:29 -0500

    Set up .h

--- HW 9/polynomial.cpp
+++ HW 9/polynomial.cpp
@@ -2,13 +2,12 @@
 Anthony Lamelas, polynomial.cpp file for polynomai linked list homework
 */
 
-
 #include <iostream>
 #include <vector>
 #include "polynomial.h"
 using namespace std;
 
 namespace linkedPolynomial{
-
     
+
 }
--- HW 9/polynomial.h
+++ HW 9/polynomial.h
@@ -12,10 +12,42 @@ Anthony Lamelas, polynomial.h file for polynomai linked list homework
 namespace linkedPolynomial{
 
     class Polynomial{
+        struct Term;
+
+        friend ostream& operator <<(std::ostream& os, const Polynomial rhs);
+        friend bool operator==(const Polynomial& lhs, const Polynomial& rhs);
 
         public:
+            Polynomial();
+            Polynomial(const std::vector<int>& coefficients);
+            
+            Polynomial& operator+=(const Polynomial& rhs);
+            
+        //Copy Control
+            Polynomial& operator=(const Polynomial& rhs);
+            Polynomial(const Polynomial& rhs);
+            ~Polynomial();
 
+        int eval(int val) const;
+
+        private:
+
+            struct NodeTerm{
+
+                Term(int coefficient = 0, Term* next = nullptr);
+
+                    int coefficient;
+                    Term* next;
+            };
+
+            Term* head;
+            int degree;
+
+            
     };
+
+    Polynomial operator+(const Polynomial& lhs, const Polynomial& rhs);
+    bool operator!=(const Polynomial& lhs, const Polynomial& rhs);
 
 }
 
